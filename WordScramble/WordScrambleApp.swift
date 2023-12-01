1da522f63eefd16bf4fcf5ab23a1fa85854894b3
RAKHYUN <kyle2598@gmail.com>
2023-12-01 14:10:50 -0600

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Rakhyun Kim on 12/1/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
